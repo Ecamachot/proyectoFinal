aa274c33e95c23185850d8eceee117298be9aaa0
Eduardo <Eduardo@LAGOS>
2018-07-01 14:59:23 -0500

    Agregar archivos de proyecto.

---
+++ DirectorioPNP2018/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DirectorioPNP2018.Models;
+
+namespace DirectorioPNP2018.Controllers
+{
+    public class HomeController : Controller
+
+    {
+        private DirectorioPNPEntities db = new DirectorioPNPEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult resultado()
+
+        {
+            string q = this.Request.Params.Get("nombreComun");
+            string d = this.Request.Params.Get("departamento");
+            return View(db.UnidadPNPs.Where(u => u.nombreComun.Contains(q) && u.departamento.Equals(d)).ToList());
+            
+
+
+        }
+    }
+
+}
